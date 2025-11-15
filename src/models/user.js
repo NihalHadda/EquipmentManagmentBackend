7b9658c82d1bc6c31199cfcd2f1ec3ce9f662390
NihalHadda <nihal.hadda@isgb.ucar.tn>
2025-11-15 23:30:24 +0100

    commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,13 @@
-const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true , trim : true},
-  username: { type: String, required: true, trim : true },
-  password: { type: String, required: true , trim : true},
-  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },});
+import mongoose from "mongoose";
 
-module.exports = mongoose.model("Users", userSchema);
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user", "superviseur"], default: "user" }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
